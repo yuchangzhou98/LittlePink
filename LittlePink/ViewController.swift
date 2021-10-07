039c217812bcbd350e014c3081bce6fb50d8ccb2
Yuchang Zhou <yuchangzhou98@gmail.com>
2021-10-07 10:32:11 +0800

    Initial Commit

---
+++ LittlePink/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LittlePink
+//
+//  Created by Yuchang Zhou on 2021/10/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
